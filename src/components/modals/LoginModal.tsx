309e1595d815d531f801a628f05aa525e2b5378c
Cameron H <cameronh@qualtrics.com>
2023-03-05 20:16:57 -0700

    styling the login modals to have better errors & login flow for choosing email/google

--- src/components/modals/LoginModal.tsx
+++ src/components/modals/LoginModal.tsx
@@ -1,5 +1,6 @@
-import React, {useRef} from "react";
-import { Modal, Button, Input, Loading, Image, Text } from "@nextui-org/react";
+import React, {useEffect, useRef, useState} from "react";
+import { Modal, Button, Input, Loading, Image, Text, Card } from "@nextui-org/react";
+import EmailIcon from '@mui/icons-material/Email';
 import { signIn } from "next-auth/react";
 import { validateSignUp } from "@/utils/validation";
 
@@ -13,7 +14,17 @@ interface LoginModalProps {
 
 const LoginModal = ({open, setOpen, isSignUp = false}: LoginModalProps) => {
     const [loggingIn, setLoggingIn] = React.useState(false);
-    const loginForm = useRef<HTMLFormElement>(null)
+    const loginForm = useRef<HTMLFormElement>(null);
+
+    const [errorMessage, setErrorMessage] = useState<string>('')
+    const [modalState, setModalState] = useState<"providers"|"email">("providers");
+
+    useEffect(() => {
+        if (open) {
+            setModalState("providers")
+            setErrorMessage("")
+        }
+    }, [open])
 
     const submitLoginForm = async (event?: any) => {
         if (!loginForm.current) return;
@@ -25,21 +36,12 @@ const LoginModal = ({open, setOpen, isSignUp = false}: LoginModalProps) => {
             const formDataJSON = Object.fromEntries(formData);
             const email = String(formDataJSON["email"]);
             const password = String(formDataJSON["password"]);
-            const errorMsg: HTMLParagraphElement | null = document.querySelector(".error-msg");
-            if (errorMsg !== null) {
-                errorMsg.textContent = ""
-                errorMsg.style.display = "none";
-            }
 
             if(isSignUp) {
                 const confirmPassword = String(formDataJSON["confirmPassword"]);
                 const errors = validateSignUp({email, password, confirmPassword});
                 if(errors.length > 0) {
-                    if(errorMsg !== null) {
-                        errorMsg.innerHTML = errors.join("<br/>");
-                        errorMsg.style.display = "block";
-                        return;
-                    }
+                    setErrorMessage(errors.join("<br/>"));
                 }
                 const response = await fetch("/api/auth/signup", {
                     method: "POST",
@@ -49,11 +51,8 @@ const LoginModal = ({open, setOpen, isSignUp = false}: LoginModalProps) => {
 
                 const data = await response.json();
                 if(response.status === 422) {
-                    if (errorMsg !== null) {
-                        errorMsg.innerHTML = data.message.replace("\n", "<br/>");
-                        errorMsg.style.display = "block";
-                        return;
-                    }
+                    setErrorMessage(data.message.replace("\n", "<br/>"));
+                    return;
                 }
             }
 
@@ -64,10 +63,7 @@ const LoginModal = ({open, setOpen, isSignUp = false}: LoginModalProps) => {
                             setOpen(false);
                         }
                         else {
-                            if (errorMsg !== null) {
-                                errorMsg.textContent = "Invalid email or password";
-                                errorMsg.style.display = "block";
-                            }
+                            setErrorMessage("Invalid email or password");
                         }
                     }
                 });
@@ -75,6 +71,83 @@ const LoginModal = ({open, setOpen, isSignUp = false}: LoginModalProps) => {
         finally {
             setLoggingIn(false);
         }
+    }
+
+    const getContentEmail = () =>  {
+        return (
+            <form ref={loginForm} id="loginForm" onSubmit={submitLoginForm}>
+                    <Input
+                        required
+                        fullWidth
+                        label="Email"
+                        placeholder=""
+                        initialValue=""
+                        inputMode="email"
+                        name="email"
+                    />
+                    <Input.Password
+                        fullWidth
+                        required
+                        label="Password"
+                        placeholder=""
+                        initialValue=""
+                        name="password"
+                    />
+                    {isSignUp?
+                    <Input.Password
+                        fullWidth
+                        required
+                        label="Confirm Password"
+                        placeholder=""
+                        initialValue=""
+                        name="confirmPassword"
+                    />
+                    :
+                    ""
+                    }
+                    <Button
+                        style={{width: "100%"}}
+                        type="submit"
+                        disabled={loggingIn}
+                        form="loginForm"
+                        css={{marginTop: "1em"}}
+                    >
+                        {loggingIn ? (
+                            <Loading type="points" />
+                        ) : (
+                            isSignUp? "Signup" : "Login"
+                        )}
+                    </Button>
+                </form>
+        )
+    }
+
+    const getContentProviders = () => {
+        return (
+            <>
+                <Button
+                    onPress={() => setModalState("email")}
+                    className="email-login-btn"
+                    bordered
+                    size="lg"
+                    iconLeftCss={{left: "12px"}}
+                    icon={<EmailIcon shapeRendering='rounded' />}
+                >
+                    {isSignUp ? "Sign up with Email" : "Login with Email"}
+                </Button>
+                <Button
+                    onPress={() => signIn("google")}
+                    className="google-login-btn"
+                    bordered
+                    size="lg"
+                    iconLeftCss={{left: "0"}}
+                    icon={<Image src="/btn_google_light_normal_ios.svg"/>}
+                >
+                    {isSignUp ? "Sign up with Google" : "Login with Google"}
+                </Button>
+            </>
+ 
+        )
     }
 
     return (
@@ -89,61 +162,23 @@ const LoginModal = ({open, setOpen, isSignUp = false}: LoginModalProps) => {
                     </Text>
                 </Modal.Header>
                 <Modal.Body>
-                <p style={{color: "red", border: "solid 1px red", backgroundColor: "#ff000011", padding: "1em", display: "none"}} className="error-msg"></p>
-                    <form ref={loginForm} id="loginForm" onSubmit={submitLoginForm}>
-                        <Input
-                            required
-                            fullWidth
-                            label="Email"
-                            placeholder=""
-                            initialValue=""
-                            inputMode="email"
-                            name="email"
-                        />
-                        <Input.Password
-                            fullWidth
-                            required
-                            label="Password"
-                            placeholder=""
-                            initialValue=""
-                            name="password"
-                        />
-                        {isSignUp?
-                        <Input.Password
-                            fullWidth
-                            required
-                            label="Confirm Password"
-                            placeholder=""
-                            initialValue=""
-                            name="confirmPassword"
-                        />
-                        :
-                        ""
-                        }
-                        <Button
-                            auto
-                            type="submit"
-                            disabled={loggingIn}
-                            form="loginForm"
-                            css={{marginTop: "1em"}}
-                        >
-                            {loggingIn ? (
-                                <Loading type="points" />
-                            ) : (
-                                isSignUp? "Signup" : "Login"
-                            )}
-                        </Button>
-                    </form>
-                    <Button
-                        onPress={() => signIn("google")}
-                        className="google-login-btn"
-                        bordered
-                        size="lg"
-                        iconLeftCss={{left: "0"}}
-                        icon={<Image src="/btn_google_light_normal_ios.svg"/>}
-                    >
-                        {isSignUp ? "Sign up with Google" : "Sign in with Google"}
-                    </Button>
+                <Card
+                    variant="bordered"
+                    css={{
+                        background: '$red100',
+                        borderColor: '$red700',
+                        display: errorMessage ? 'block' : 'none'
+                    }}>
+                    <Card.Body>
+                        <Text css={{color: '$red700'}} dangerouslySetInnerHTML={{__html: errorMessage}} />
+                    </Card.Body>
+                </Card>
+                    {modalState == "providers" ? (
+                        getContentProviders()
+                    ) : ('')}
+                    {modalState == "email" ? (
+                        getContentEmail()
+                    ) : ('')}
                 </Modal.Body>
                 <Modal.Footer>
                     <Button
