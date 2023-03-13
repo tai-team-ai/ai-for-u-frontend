d98c35240e75158dbbc0fee1c06271f65cbb3e23
mikeswhitney33 <mikeswhitney33@gmail.com>
2023-03-13 12:41:54 -0400

    removed stubbed sandbox chat

--- src/pages/api/ai-for-u/sandbox-chatgpt.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-// TODO: remove this once the endpoint is working
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).json({gptResponse: req.body.userMessage});
-}
