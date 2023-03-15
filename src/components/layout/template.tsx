204839125176e450e91c51a7869e359608fd594d
mikeswhitney33 <mikeswhitney33@gmail.com>
2023-03-15 01:40:53 -0400

    Chat Examples Added a templitized examples section implemented the fillExamples function for the chat updated the next-auth library due to it being a security update

--- src/components/layout/template.tsx
+++ src/components/layout/template.tsx
@@ -1,17 +1,80 @@
 import styles from '@/styles/Template.module.css'
 import {routes} from '@/utils/constants'
-import { Grid, Text } from '@nextui-org/react'
+import { uFetch } from '@/utils/http'
+import { Card, Grid, Text } from '@nextui-org/react'
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
-import { TemplateCards } from '../elements/TemplateCard'
+import { PropsWithChildren, useState } from 'react'
 
 
 interface TemplateProps {
     isSandbox?: boolean
-    children?: React.ReactNode
+    children?: React.ReactNode,
+    exampleUrl?: string | null
+    fillExample?: ((e: any) => void) | null
+}
+
+interface ExampleObject {
+    name: string;
+    example: any;
+}
+
+interface ExampleProps {
+    example: any;
+    fillExample: ((e: any) => void) | null
+}
+
+const Example = ({example, fillExample, ...props}: PropsWithChildren<ExampleProps>) => {
+    return (
+        <>
+        <Card
+                isPressable
+                isHoverable
+                variant="bordered"
+                className={styles["template-card"]}
+                disableRipple={true} // if this page is turned into a single page app, then we'd want to enable this again
+                css={{ h: "$24", $$cardColor: '$colors$primary' }}
+                onPress={() => {fillExample ? fillExample(example) : null}}
+                >
+                    <Card.Body css={{display: 'flex', justifyContent: "center", alignItems: "center"}}>
+                        <Text
+                            size="$xl"
+                            weight="bold"
+                            color="white" >
+                            {props.children}
+                        </Text>
+                    </Card.Body>
+            </Card>
+        </>
+    )
 }
 
 
-export default function Template({isSandbox = false, children = null}: TemplateProps) {
+export default function Template({isSandbox = false, children = null, exampleUrl = null, fillExample = null}: TemplateProps) {
+    const {data: session} = useSession();
+    const [examples, setExamples] = useState<ExampleObject[]>([]);
+    const [loading, setLoading] = useState(false);
+
+    if(exampleUrl && !loading) {
+        setLoading(true)
+        uFetch(exampleUrl, {
+            session: session,
+            method: "GET",
+        }).then(res => {
+            return res.json();
+        }).then(data => {
+            for(let i = 0;i < data.exampleNames.length;i++) {
+                examples.push({
+                    name: data.exampleNames[i],
+                    example: data.examples[i]
+                });
+            }
+            setExamples([...examples]);
+        }).catch(error => {
+
+        });
+    }
+
     return (
         <Grid.Container gap={1} direction="row-reverse">
             <Grid sm={9} xs={12}>
@@ -28,21 +91,20 @@ export default function Template({isSandbox = false, children = null}: TemplateP
                     <div className={styles["examples"]}>
                         <Grid.Container gap={1} justify='flex-start'>
                         {
-                            TemplateCards.map((c, i) => {
-                                return (
-                                    <Grid key={i} xs={12}>
-                                        {c}
-                                    </Grid>
-                                )
-                            })
+                            examples.length > 0 ?
+                            examples.map((example) => {
+                                return <Example
+                                    example={example.example}
+                                    fillExample={fillExample}>
+                                        {example.name}
+                                    </Example>
+                            }) : "Examples coming soon..."
                         }
                         </Grid.Container>
                     </div>
                 </section>
             </Grid>
 
-            
         </Grid.Container>
-        
     )
 }
