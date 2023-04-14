929f4be747b7e77eccbae6cfff1a7136f5fc8679
Jacob Petterle <jacobpetterle+aiforu@gmail.com>
2023-04-14 14:23:19 +0000

    Organize and style footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,39 +1,43 @@
 import React from 'react'
-import { Container, Grid, Text, Image, Link } from '@nextui-org/react'
+import { Container, Grid, Text, Image, Link, useModal } from '@nextui-org/react'
 import { routes } from '@/utils/constants'
 import styles from '@/styles/Footer.module.css'
 import SubscribeField from '../elements/SubscribeField'
+import GoProModal from '@/components/modals/GoProModal'
+
 
 const Footer = (): JSX.Element => {
+  const { setVisible: setShowGoProModal, bindings: goProBindings } = useModal()
   return (
         <React.Fragment>
             <footer className={styles.footer}>
-                <Container fluid css={{ borderTop: 'solid 1px $colors$primary' }}>
-                    <Container>
-                        <Grid.Container>
-                            <Grid xs={12} sm={12} md={6} lg={4}>
-                                <ul>
-                                    <Text h3>Pages</Text>
-                                    <li><Link css={{ color: '$colors$primary' }} href={routes.SANDBOX}>Sandbox</Link></li>
-                                    <li><Link css={{ color: '$colors$primary' }} href={routes.TEMPLATES}>Templates</Link></li>
-                                </ul>
-                            </Grid>
-                            <Grid xs={12} sm={12} md={6} lg={4} css={{ textAlign: 'center', paddingTop: '1em' }}>
-                                <SubscribeField />
-                            </Grid>
-                            <Grid>
-                                <Image
-                                    src="/logo-full.png"
-                                    width={100}
-                                />
-                            </Grid>
-                        </Grid.Container>
-                        <Text css={{ textAlign: 'center' }}>
-                            &copy; AIforU - 2022
-                        </Text>
-                    </Container>
+                <Container fluid css={{ borderTop: 'solid 1px $colors$primary', marginTop: '2em' }}>
+                  <Container>
+                    <Grid.Container gap={2} justify='space-between'>
+                      <Grid justify='flex-start' css={{ marginLeft: '3em' }}>
+                        <ul>
+                          <Text h3 css={{ lineHeight: '2em' }}>Pages</Text>
+                          <li><Link css={{ color: '$colors$primary' }} href={routes.SANDBOX}>Sandbox</Link></li>
+                          <li><Link css={{ color: '$colors$primary' }} href={routes.TEMPLATES}>Templates</Link></li>
+                          <li><Link css={{ color: '$colors$primary' }} onClick={() => { setShowGoProModal(true) }}>GoPro</Link></li>
+                        </ul>
+                      </Grid>
+                      <Grid justify='flex-end' css={{ marginRight: '-3em', marginTop: '1em' }}>
+                        <Image
+                          src="/logo-full-aligned-right.png"
+                          width={180}
+                          css={{ marginBottom: '1.5em' }}
+                        />
+                        <SubscribeField style={{ marginRight: '11em' }} />
+                      </Grid>
+                    </Grid.Container>
+                    <Text css={{ textAlign: 'center' }}>
+                      &copy; AIforU - 2022
+                    </Text>
+                  </Container>
                 </Container>
             </footer>
+            <GoProModal bindings={goProBindings} />
         </React.Fragment>
   )
 }
