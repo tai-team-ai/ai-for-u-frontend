046ee4d7e0b9ee4584fc8f97ed45cb09a6ce41cc
Jacob Petterle <jacobpetterle+aiforu@gmail.com>
2023-04-14 16:22:45 +0000

    Add hook to detect the viewport size

---
+++ src/utils/hooks.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+
+export const useViewport = (): { width: number, height: number } => {
+  const [viewport, setViewport] = useState({
+    width: typeof window !== 'undefined' ? window.innerWidth : 0,
+    height: typeof window !== 'undefined' ? window.innerHeight : 0
+  })
+
+  useEffect(() => {
+    const handleResize = (): void => {
+      setViewport({
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return viewport
+}
