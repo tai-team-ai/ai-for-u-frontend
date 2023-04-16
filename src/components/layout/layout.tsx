5cdb1e94b18ec3d3746b1c6bd6c75c989b4cc59d
Jacob Petterle <jacobpetterle+aiforu@gmail.com>
2023-04-16 01:08:50 +0000

    update the hiehgt and remove space above allpages

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -8,37 +8,37 @@ interface LayoutProps {
   children: any
 }
 
+const theme = createTheme({
+  type: 'light', // it could be "light" or "dark"
+  theme: {
+    // generated using https://colors.eva.design/
+    colors: {
+      primaryLight: '#CAB1EF', // originally $blue200
+      primaryLightHover: '#9D7FD1', // originally $blue300
+      primaryLightActive: '#6E54A3', // originally $blue400
+      primaryLightContrast: '#2B1A57', // originally $blue600
+      primary: '#2B1A57', // originally $blue600
+      primaryBorder: '#382466', // originally $blue500
+      primaryBorderHover: '#2B1A57', // originally $blue600
+      primarySolidHover: '#1F1249', // originally $blue700
+      primarySolidContrast: '$white', // originally $white
+      primaryShadow: '#382466' // originally $blue500
+    },
+    space: {},
+    fonts: {}
+  }
+})
+
 function Layout ({ children }: LayoutProps): JSX.Element {
-  const theme = createTheme({
-    type: 'light', // it could be "light" or "dark"
-    theme: {
-      // generated using https://colors.eva.design/
-      colors: {
-        primaryLight: '#CAB1EF', // originally $blue200
-        primaryLightHover: '#9D7FD1', // originally $blue300
-        primaryLightActive: '#6E54A3', // originally $blue400
-        primaryLightContrast: '#2B1A57', // originally $blue600
-        primary: '#2B1A57', // originally $blue600
-        primaryBorder: '#382466', // originally $blue500
-        primaryBorderHover: '#2B1A57', // originally $blue600
-        primarySolidHover: '#1F1249', // originally $blue700
-        primarySolidContrast: '$white', // originally $white
-        primaryShadow: '#382466' // originally $blue500
-      },
-      space: {},
-      fonts: {}
-    }
-  })
   return (
     <SSRProvider>
       <NextUIProvider theme={theme}>
           <PageNavbar />
-          <Spacer y={1} />
           <Container css={{
             '@media screen and (min-width: 1280px)': {
               maxWidth: '1430px'
             },
-            paddingBottom: '5rem', // This will create space at the bottom of the container for the footer.
+            paddingBottom: '1rem', // This will create space at the bottom of the container for the footer.
             minHeight: '100%' // This will make the container grow with its contents.
           }}>
             {children}
