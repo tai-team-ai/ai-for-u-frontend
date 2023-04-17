fbfc2a461fbd50086cb65a21e0b75e62c15caced
Jacob Petterle <jacobpetterle+aiforu@gmail.com>
2023-04-17 05:19:43 +0000

    update ccards to link to pages and modals

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import { routes } from '@/utils/constants'
 import Layout from '@/components/layout/layout'
 import AIForAnimation from '@/components/elements/AIForAnimation'
@@ -7,6 +7,7 @@ import styles from '@/styles/Home.module.css'
 import Image from 'next/image'
 import { Container, Row, Spacer, Button, Link, Grid } from '@nextui-org/react'
 import FancyHoverCard from '@/components/elements/FancyHoverCard'
+import SubscribeModal from '@/components/modals/SubscribeModal'
 import { isMobile } from '@/utils/hooks'
 
 const HERO_DESCRIPTION: string = 'Super charge your life with powerful AI templates & tools! 🚀'
@@ -14,28 +15,37 @@ const HERO_DESCRIPTION: string = 'Super charge your life with powerful AI templa
 const TRUST_BUILDERS = [
   {
     title: 'AI Assistant (ChatGPT)',
-    description: 'Identical to ChatGPT, without the wait! You will be blown away by the possibilities! From generating content ideas, to writing emails, to just chatting, we\'ve got you covered!',
-    hover: 'Let\'s Chat! 😊'
+    description: 'Identical to ChatGPT, without the wait! You will be blown away by the functionality that this tool offers! From generating content ideas, to writing emails, to just chatting, we\'ve got you covered!',
+    hover: 'Let\'s Chat! 😊',
+    link: routes.SANDBOX,
+    subscribeModal: false
   },
   {
     title: 'Chrome Gmail Extension',
     description: 'Tired of writing emails? Let AI do it for you! Our Chrome extension fully automates the process of writing emails! We\'re really excited to bring this to you!',
-    hover: 'Coming Soon! 🙌'
+    hover: 'Coming Soon! 🙌',
+    link: null,
+    subscribeModal: true
   },
   {
     title: 'Baby Name Generator',
     description: 'We\'ve heard you, and we\'re working on it! Our AI will help you find the perfect name while your expecting or dreaming of that perfect name!',
-    hover: 'Almost there! 🎊'
+    hover: 'Almost there! 🎊',
+    link: null,
+    subscribeModal: true
   },
   {
     title: 'Mobile AI Tools Application',
     description: 'We\'re working on bringing our AI tools to your mobile devices! We will be offering powerful multi-modal AI tools for your mobile devices!',
-    hover: 'Heck yeah! 🤩'
+    hover: 'Heck yeah! 🤩',
+    link: null,
+    subscribeModal: true
   }
 ]
 
 function Home (): JSX.Element {
   const isMobileDisplay: boolean = isMobile()
+  const [open, setOpen] = useState(false)
   return (
         <Layout>
             <section className={styles['hero-section']}>
@@ -89,12 +99,23 @@ function Home (): JSX.Element {
               <Grid.Container gap={2.6}>
                 {TRUST_BUILDERS.map((TRUST_BUILDERS, index) => (
                   <Grid xs={12} sm={6} md={6} key={index}>
-                    <FancyHoverCard
-                      size="lg"
-                      title={TRUST_BUILDERS.title}
-                      description={TRUST_BUILDERS.description}
-                      hover={TRUST_BUILDERS.hover}
-                    />
+                    <Link
+                          href={TRUST_BUILDERS.link != null ? TRUST_BUILDERS.link : '#'}
+                          style={{ height: '100%', width: '100%' }}
+                          onClick={(e) => {
+                            if (TRUST_BUILDERS.subscribeModal) {
+                              e.preventDefault()
+                              setOpen(true)
+                            }
+                          }}
+                      >
+                      <FancyHoverCard
+                        size="lg"
+                        title={TRUST_BUILDERS.title}
+                        description={TRUST_BUILDERS.description}
+                        hover={TRUST_BUILDERS.hover}
+                      />
+                    </Link>
                   </Grid>
                 ))}
               </Grid.Container>
@@ -112,6 +133,7 @@ function Home (): JSX.Element {
                     <Image src="" alt=""/>
                 </div>
             </section>
+            <SubscribeModal open={open} setOpen={setOpen} />
         </Layout>
   )
 }
