0d28b0762377edeab4f254d45878a84bcd4bee09
mikeswhitney33 <mikeswhitney33@gmail.com>
2023-04-26 22:23:46 -0400

    added capability to change the styles of markdown objects

--- src/components/elements/Markdown.tsx
+++ src/components/elements/Markdown.tsx
@@ -1,8 +1,13 @@
 import MarkdownToJSX from 'markdown-to-jsx'
 import { type PropsWithChildren } from 'react'
+import styles from '@/styles/Markdown.module.css'
 
 const Markdown = ({ children }: PropsWithChildren): JSX.Element => {
-  return <MarkdownToJSX>{children as string}</MarkdownToJSX>
+  return (
+    <div className={styles.markdown}>
+      <MarkdownToJSX options={{}}>{children as string}</MarkdownToJSX>
+    </div>
+  )
 }
 
 export default Markdown
