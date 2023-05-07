0875453376b044a23219641391618d300816fe98
mikeswhitney33 <mikeswhitney33@gmail.com>
2023-05-06 21:33:02 -0400

    changed gptResponse to response and removed a reference to prefetching that used it

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,35 +1,5 @@
 import { type ExampleObject } from '@/components/layout/template'
 import { v4 as uuid } from 'uuid'
-import { uFetch } from './http'
-
-const prefetchInitialChat = async (): Promise<string> => {
-  return await uFetch('/api/ai-for-u/sandbox-chatgpt', {
-    method: 'POST',
-    body: JSON.stringify({
-      conversationUuid: uuid(),
-      userMessage: ''
-    })
-  }).then(async response => {
-    return await response.json()
-  }).then(data => {
-    sessionStorage.setItem('initialChat', data.gptResponse)
-    return data.gptResponse
-  })
-}
-
-export async function getInitialChat (): Promise<string> {
-  if (typeof sessionStorage === 'undefined') {
-    return 'no chat yet'
-  }
-  let initialChat = sessionStorage.getItem('initialChat')
-  if (initialChat == null) {
-    initialChat = await prefetchInitialChat()
-    if (initialChat.length === 0) {
-      return 'something went wrong'
-    }
-  }
-  return initialChat
-}
 
 // client-side function
 export function getUserID (): string {
